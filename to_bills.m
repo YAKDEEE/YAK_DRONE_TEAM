d8b27700154aeaaa241aa4533214fcc04b96293f
ChanByeongee <well87865@gmail.com>
2022-04-18 01:07:47 +0900

    Modified to_bills function

--- to_bills.m
+++ to_bills.m
@@ -12,19 +12,20 @@ bills_unit=["$","€","¥(Yen)","¥(Yuan)"];
 
 for currency_idx = 1:4
     changed_money=curreny(currency_idx);
-
+    sum = 0;
     for bills_idx = 1:10
         money=bills_matrix(currency_idx,bills_idx);
 
         if money ~= 0
             if money <= changed_money
+               sum= sum+fix(changed_money/money);
                fprintf("%d%s짜리 화폐 : %d개 ",money,bills_unit(currency_idx),fix(changed_money/money));
             end
 
             changed_money= rem(changed_money,money);
         end
     end
-    fprintf("\n");
+    fprintf("\n총 화폐(%s) 갯수: %d개\n",bills_unit(currency_idx),sum);
 end
 
 
