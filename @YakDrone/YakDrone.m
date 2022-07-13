769951ee10b30a8675eba5371ec85f63fabbf295
YAKDEEE <yongsuk981@naver.com>
2022-07-13 17:36:32 +0900

    YS

--- @YakDrone/YakDrone.m
+++ @YakDrone/YakDrone.m
@@ -13,7 +13,8 @@ classdef YakDrone < handle
         cCircle_th = 0.45;
         cCircle_rad_th = 50;
         cCircle_Max_ecc_th = 0.85;
-        cCircle_Min_ecc_th = 0.5;
+       % cCircle_Min_ecc_th = 0.5;
+        cOptimized_ratio_th = 0.5; % 단축/장축 비율th 
         cTotal_Circle = 3;
         cCircle_size = [0.39,0.28,0.25];
         cRange_th = 60; 
@@ -44,6 +45,7 @@ classdef YakDrone < handle
         nEccentricity=0;
         nFinder=0;
         nMoveWeight=0;
+        nRatio = 0; %단축/장축
         
         is_last_we_had_positioned = 0;
 
